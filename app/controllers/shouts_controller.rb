220e7a15a0ce149293d69143486d8bebaa2a5f3b
Brian McQueen <brian.d.mcqueen@gmail.com>
2014-10-13 10:59:14 -0400

    Require log in; users can create shouts

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -1,5 +1,17 @@
 class ShoutsController < ApplicationController
+  before_action :require_login
   def index
+    @shout = Shout.new
     @shouts = Shout.all
+  end
+
+  def create
+    @shout = Shout.new(shout_params)
+    @shout.user_id = current_user.id
+    redirect_to shouts_path
+  end
+  private
+  def shout_params
+    params.require(:shout).permit(:user_id, :body)
   end
 end

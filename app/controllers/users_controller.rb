750752ab0b213968a3236fe28efc57771bd2d92f
Brian McQueen <brian.d.mcqueen@gmail.com>
2014-10-14 13:44:22 -0400

    Adds user#show action
    User#show page now lists the people user is following, and people following user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,12 @@ class UsersController < ApplicationController
     @following_relationship = FollowingRelationship.new
   end
 
+  def show
+    @user = User.find(params[:id])
+    @followed_users = @user.followed_users
+    @followers = @user.followers
+  end
+
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "shouts#index"
   resources :shouts, only: [:create, :index]
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :index] do
+  resources :users, only: [:new, :create, :index, :show] do
     resource :following_relationship, only: [:create, :destroy]
   end
 end
